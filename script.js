62cfc4f736f3f628c4de5740fe61cee4c89a20a7
shawn <173310929+shawntarou@users.noreply.github.com>
2024-08-10 16:35:44 -0700

    Create functions for basic math operations

---
+++ script.js
@@ -0,0 +1,21 @@
+function add(firstNum, secondNum) {
+    return firstNum + secondNum;
+}
+
+function subtract(firstNum, secondNum) {
+    return firstNum - secondNum;
+}
+
+function multiply(firstNum, secondNum) {
+    return firstNum * secondNum;
+}
+
+function divide(firstNum, secondNum) {
+    return firstNum / secondNum;
+}
+
+
+console.log(add(1, 2));
+console.log(subtract(4, 2));
+console.log(multiply(5, 4));
+console.log(divide(4, 2));
