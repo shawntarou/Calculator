5244e7a3c1500f17e451cd6bece332932c3191c4
shawn <173310929+shawntarou@users.noreply.github.com>
2024-08-11 16:20:00 -0700

    Link basic add, equals, and clear functionality with UI

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-function add(x, y) {
+function addOp(x, y) {
     return x + y;
 }
 
-function subtract(x, y) {
+function subtractOp(x, y) {
     return x - y;
 }
 
-function multiply(x, y) {
+function multiplyOp(x, y) {
     return x * y;
 }
 
-function divide(x, y) {
+function divideOp(x, y) {
     return x / y;
 }
 
@@ -18,39 +18,43 @@ let firstNum;
 let seccondNum;
 let operator;
 
+const displayNumber = document.querySelector("#display-number");
+
 function operate() {
-    let firstNum = parseInt(prompt("first?"));
-    let operator = prompt("operator?");
-    let secondNum = parseInt(prompt("second?"));
     switch (operator) {
         case 'add':
-            console.log(add(firstNum, secondNum));
+            const result = addOp(firstNum, secondNum);
+            console.log(result);
+            displayNumber.textContent = result;
+            currentValue = result;
+            firstNum = 0;
+            secondNum = 0;
             break;
         case 'subtract':
-            console.log(subtract(firstNum, secondNum));
+            console.log(subtractOp(firstNum, secondNum));
             break;
         case 'multiply':
-            console.log(multiply(firstNum, secondNum));
+            console.log(multiplyOp(firstNum, secondNum));
             break;
         case 'divide':
-            console.log(divide(firstNum, secondNum));
+            console.log(divideOp(firstNum, secondNum));
             break;
     };
 };
 
-const displayValue = document.querySelector("#display-number")
-let currentValue;
+const displayValue = document.querySelector("#display-number");
+let currentValue = 0;
 
-const numButtons = document.querySelector(".num-buttons")
-const one = document.querySelector("#one-button")
-const two = document.querySelector("#two-button")
-const three = document.querySelector("#three-button")
-const four = document.querySelector("#four-button")
-const five = document.querySelector("#five-button")
-const six = document.querySelector("#six-button")
-const seven = document.querySelector("#seven-button")
-const eight = document.querySelector("#eight-button")
-const nine = document.querySelector("#nine-button")
+const one = document.querySelector("#one-button");
+const two = document.querySelector("#two-button");
+const three = document.querySelector("#three-button");
+const four = document.querySelector("#four-button");
+const five = document.querySelector("#five-button");
+const six = document.querySelector("#six-button");
+const seven = document.querySelector("#seven-button");
+const eight = document.querySelector("#eight-button");
+const nine = document.querySelector("#nine-button");
+const zero = document.querySelector("#zero-button");
 
 one.addEventListener("click", function () {
     if (displayValue.textContent == 0) {
@@ -142,18 +146,60 @@ nine.addEventListener("click", function () {
     }
     console.log(currentValue);
 })
+zero.addEventListener("click", function () {
+    if (displayValue.textContent == 0) {
+        currentValue = "0";
+        displayValue.textContent = currentValue;
+    } else {
+        currentValue = currentValue + "0";
+        displayValue.textContent = currentValue;
+    }
+    console.log(currentValue);
+})
 
+const allClear = document.querySelector("#all-clear-button");
+const add = document.querySelector("#add-button");
+const subtract = document.querySelector("#subtract-button");
+const multiply = document.querySelector("#multiply-button");
+const divide = document.querySelector("#divide-button");
+const equals = document.querySelector("#equals-button");
 
+let operationCheck = false;
+let clickCounter = 0;
 
+allClear.addEventListener("click", function () {
+    currentValue = 0;
+    displayValue.textContent = currentValue;
+    console.log(currentValue);
+})
 
-
-
-
-
-
-
-
-
-function displayNum() {
-
+function clearDisplay() {
+    displayValue.textContent = 0;
+    currentValue = 0;
+    console.log("First Num:" + firstNum);
+    console.log("curr val:" + currentValue);
 }
+
+add.addEventListener("click", function () {
+    if (operationCheck) {
+        secondNum = Number(currentValue);
+        operate();
+        operationCheck = false;
+    }
+    firstNum = Number(currentValue);
+    operator = 'add';
+    console.log("add");
+    currentValue = 0;
+    operationCheck = true;
+})
+
+equals.addEventListener("click", function () {
+    secondNum = Number(currentValue);
+    operate();
+})
+
+
+
+
+
+
