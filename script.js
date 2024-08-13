b4d011027db816c5acda0b336568e89b1abed311
shawn <173310929+shawntarou@users.noreply.github.com>
2024-08-12 17:41:46 -0700

    Link sub, mul, and div with their respective DOM elements

--- script.js
+++ script.js
@@ -15,29 +15,52 @@ function divideOp(x, y) {
 }
 
 let firstNum;
-let seccondNum;
+let secondNum;
 let operator;
+let result;
 
 const displayNumber = document.querySelector("#display-number");
 
 function operate() {
     switch (operator) {
         case 'add':
-            const result = addOp(firstNum, secondNum);
+            result = addOp(firstNum, secondNum);
             console.log(result);
             displayNumber.textContent = result;
-            currentValue = result;
-            firstNum = 0;
+            currentValue = 0;
+            firstNum = 0 + result;
             secondNum = 0;
             break;
         case 'subtract':
-            console.log(subtractOp(firstNum, secondNum));
+            result = subtractOp(firstNum, secondNum);
+            console.log(result);
+            displayNumber.textContent = result;
+            currentValue = 0;
+            firstNum = 0 + result;
+            secondNum = 0;
             break;
         case 'multiply':
-            console.log(multiplyOp(firstNum, secondNum));
+            result = multiplyOp(firstNum, secondNum);
+            console.log(result);
+            displayNumber.textContent = result;
+            currentValue = 0;
+            firstNum = 0 + result;
+            secondNum = 0;
             break;
         case 'divide':
-            console.log(divideOp(firstNum, secondNum));
+            result = divideOp(firstNum, secondNum);
+            console.log(result);
+            if (result == Infinity) {
+                displayNumber.textContent = "( •_•)";
+                currentValue = 0;
+                firstNum = 0;
+                secondNum = 0;
+            } else {
+                displayNumber.textContent = result;
+                currentValue = 0;
+                firstNum = 0 + result;
+                secondNum = 0;
+            }
             break;
     };
 };
@@ -57,102 +80,205 @@ const nine = document.querySelector("#nine-button");
 const zero = document.querySelector("#zero-button");
 
 one.addEventListener("click", function () {
-    if (displayValue.textContent == 0) {
+    if (newOperationCheck == true) {
         currentValue = "1";
         displayValue.textContent = currentValue;
-    } else {
-        currentValue = currentValue + 1;
+        newOperationCheck = false;
+    }
+    else if (displayValue.textContent == 0) {
+        currentValue = "1";
         displayValue.textContent = currentValue;
+    }
+    else {
+        if (currentValue.length > 7) {
+            return;
+        } else {
+            currentValue = String(currentValue + 1);
+            displayValue.textContent = currentValue;
+        }
     }
     console.log(currentValue);
 })
 two.addEventListener("click", function () {
-    if (displayValue.textContent == 0) {
+    if (newOperationCheck == true) {
         currentValue = "2";
         displayValue.textContent = currentValue;
-    } else {
-        currentValue = currentValue + 2;
+        newOperationCheck = false;
+    }
+    else if (displayValue.textContent == 0) {
+        currentValue = "2";
         displayValue.textContent = currentValue;
+    }
+    else {
+        if (currentValue.length > 7) {
+            return;
+        } else {
+            currentValue = String(currentValue + 2);
+            displayValue.textContent = currentValue;
+        }
     }
     console.log(currentValue);
 })
 three.addEventListener("click", function () {
-    if (displayValue.textContent == 0) {
+    if (newOperationCheck == true) {
         currentValue = "3";
         displayValue.textContent = currentValue;
-    } else {
-        currentValue = currentValue + 3;
+        newOperationCheck = false;
+    }
+    else if (displayValue.textContent == 0) {
+        currentValue = "3";
         displayValue.textContent = currentValue;
+    }
+    else {
+        if (currentValue.length > 7) {
+            return;
+        } else {
+            currentValue = String(currentValue + 3);
+            displayValue.textContent = currentValue;
+        }
     }
     console.log(currentValue);
 })
 four.addEventListener("click", function () {
-    if (displayValue.textContent == 0) {
+    if (newOperationCheck == true) {
         currentValue = "4";
         displayValue.textContent = currentValue;
-    } else {
-        currentValue = currentValue + 4;
+        newOperationCheck = false;
+    }
+    else if (displayValue.textContent == 0) {
+        currentValue = "4";
         displayValue.textContent = currentValue;
+    }
+    else {
+        if (currentValue.length > 7) {
+            return;
+        } else {
+            currentValue = String(currentValue + 4);
+            displayValue.textContent = currentValue;
+        }
     }
     console.log(currentValue);
 })
 five.addEventListener("click", function () {
-    if (displayValue.textContent == 0) {
+    if (newOperationCheck == true) {
         currentValue = "5";
         displayValue.textContent = currentValue;
-    } else {
-        currentValue = currentValue + 5;
+        newOperationCheck = false;
+    }
+    else if (displayValue.textContent == 0) {
+        currentValue = "5";
         displayValue.textContent = currentValue;
+    }
+    else {
+        if (currentValue.length > 7) {
+            return;
+        } else {
+            currentValue = String(currentValue + 5);
+            displayValue.textContent = currentValue;
+        }
     }
     console.log(currentValue);
 })
 six.addEventListener("click", function () {
-    if (displayValue.textContent == 0) {
+    if (newOperationCheck == true) {
         currentValue = "6";
         displayValue.textContent = currentValue;
-    } else {
-        currentValue = currentValue + 6;
+        newOperationCheck = false;
+    }
+    else if (displayValue.textContent == 0) {
+        currentValue = "6";
         displayValue.textContent = currentValue;
+    }
+    else {
+        if (currentValue.length > 7) {
+            return;
+        } else {
+            currentValue = String(currentValue + 6);
+            displayValue.textContent = currentValue;
+        }
     }
     console.log(currentValue);
 })
 seven.addEventListener("click", function () {
-    if (displayValue.textContent == 0) {
+    if (newOperationCheck == true) {
         currentValue = "7";
         displayValue.textContent = currentValue;
-    } else {
-        currentValue = currentValue + 7;
+        newOperationCheck = false;
+    }
+    else if (displayValue.textContent == 0) {
+        currentValue = "7";
         displayValue.textContent = currentValue;
+    }
+    else {
+        if (currentValue.length > 7) {
+            return;
+        } else {
+            currentValue = String(currentValue + 7);
+            displayValue.textContent = currentValue;
+        }
     }
     console.log(currentValue);
 })
 eight.addEventListener("click", function () {
-    if (displayValue.textContent == 0) {
+    if (newOperationCheck == true) {
         currentValue = "8";
         displayValue.textContent = currentValue;
-    } else {
-        currentValue = currentValue + 8;
+        newOperationCheck = false;
+    }
+    else if (displayValue.textContent == 0) {
+        currentValue = "8";
         displayValue.textContent = currentValue;
+    }
+    else {
+        if (currentValue.length > 7) {
+            return;
+        } else {
+            currentValue = String(currentValue + 8);
+            displayValue.textContent = currentValue;
+        }
     }
     console.log(currentValue);
 })
 nine.addEventListener("click", function () {
-    if (displayValue.textContent == 0) {
+    if (newOperationCheck == true) {
         currentValue = "9";
         displayValue.textContent = currentValue;
-    } else {
-        currentValue = currentValue + 9;
+        newOperationCheck = false;
+    }
+    else if (displayValue.textContent == 0) {
+        currentValue = "9";
         displayValue.textContent = currentValue;
+    }
+    else {
+        if (currentValue.length > 7) {
+            return;
+        } else {
+            currentValue = String(currentValue + 9);
+            displayValue.textContent = currentValue;
+        }
     }
     console.log(currentValue);
 })
 zero.addEventListener("click", function () {
-    if (displayValue.textContent == 0) {
+    if (newOperationCheck == true) {
         currentValue = "0";
         displayValue.textContent = currentValue;
-    } else {
-        currentValue = currentValue + "0";
+        newOperationCheck = false;
+    }
+    else if (displayValue.textContent == 0) {
+        currentValue = "0";
         displayValue.textContent = currentValue;
+    }
+    else if (currentValue == 0) {
+        displayValue.textContent = currentValue;
+    }
+    else {
+        if (currentValue.length > 7) {
+            return;
+        } else {
+            currentValue = currentValue + "0";
+            displayValue.textContent = currentValue;
+        }
     }
     console.log(currentValue);
 })
@@ -164,12 +290,19 @@ const multiply = document.querySelector("#multiply-button");
 const divide = document.querySelector("#divide-button");
 const equals = document.querySelector("#equals-button");
 
+
+// Makes sure that an operation completes before another can start (E.g. 1 + 1 + 1 --> 1 + 1 = 2 + 1)
 let operationCheck = false;
-let clickCounter = 0;
+
+// Checks if an operation has already been completed allowing the user to start a new operation w/o clearing
+let newOperationCheck = false;
+
 
 allClear.addEventListener("click", function () {
     currentValue = 0;
     displayValue.textContent = currentValue;
+    operationCheck = false;
+    newOperationCheck = false;
     console.log(currentValue);
 })
 
@@ -184,18 +317,78 @@ add.addEventListener("click", function () {
     if (operationCheck) {
         secondNum = Number(currentValue);
         operate();
-        operationCheck = false;
     }
-    firstNum = Number(currentValue);
+    else {
+        if (!newOperationCheck == true) {
+            firstNum = Number(currentValue);
+        }
+        operator = 'add';
+        console.log("add");
+        currentValue = 0;
+        operationCheck = true;
+    }
     operator = 'add';
-    console.log("add");
-    currentValue = 0;
-    operationCheck = true;
+})
+subtract.addEventListener("click", function () {
+    if (operationCheck) {
+        secondNum = Number(currentValue);
+        operate();
+    }
+    else {
+        if (!newOperationCheck == true) {
+            firstNum = Number(currentValue);
+        }
+        operator = 'subtract';
+        console.log("subtract");
+        currentValue = 0;
+        operationCheck = true;
+    }
+    operator = 'subtract';
+})
+multiply.addEventListener("click", function () {
+    if (operationCheck) {
+        secondNum = Number(currentValue);
+        operate();
+    }
+    else {
+        if (!newOperationCheck == true) {
+            firstNum = Number(currentValue);
+        }
+        operator = 'multiply';
+        console.log("multiply");
+        currentValue = 0;
+        operationCheck = true;
+    }
+    operator = 'multiply';
+})
+divide.addEventListener("click", function () {
+    if (operationCheck) {
+        secondNum = Number(currentValue);
+        operate();
+    }
+    else {
+        if (!newOperationCheck == true) {
+            firstNum = Number(currentValue);
+        }
+        operator = 'divide';
+        console.log("divide");
+        currentValue = 0;
+        operationCheck = true;
+    }
+    operator = 'divide';
 })
 
 equals.addEventListener("click", function () {
-    secondNum = Number(currentValue);
-    operate();
+    if (!firstNum) {
+        return;
+    }
+    else {
+        secondNum = Number(currentValue);
+        operate();
+        operator = undefined;
+        newOperationCheck = true;
+        operationCheck = false;
+    }
 })
 
 
